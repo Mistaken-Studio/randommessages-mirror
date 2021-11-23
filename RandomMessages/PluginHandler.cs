1e806ca36b88cf996bec763443b19f8c09446a7e
Wydział Wewnętrzny <g_sw@poczta.fm>
2021-11-23 17:05:07 +0100

    Dodaj pliki projektów.

---
+++ RandomMessages/PluginHandler.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PluginHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using System;
+
+namespace Mistaken.RandomMessages
+{
+    /// <inheritdoc/>
+    public class PluginHandler : Plugin<Config>
+    {
+        /// <inheritdoc/>
+        public override string Author => "Mistaken Devs";
+
+        /// <inheritdoc/>
+        public override string Name => "";
+
+        /// <inheritdoc/>
+        public override string Prefix => "M";
+
+        /// <inheritdoc/>
+        public override PluginPriority Priority => PluginPriority.Higher;
+
+        /// <inheritdoc/>
+        public override Version RequiredExiledVersion => new Version(2, 11, 0);
+
+        /// <inheritdoc/>
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            // new Handler(this);
+
+            API.Diagnostics.Module.OnEnable(this);
+
+            base.OnEnabled();
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisabled()
+        {
+            API.Diagnostics.Module.OnDisable(this);
+
+            base.OnDisabled();
+        }
+
+        internal static PluginHandler Instance { get; private set; }
+    }
+}
